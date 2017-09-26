ecd3983aa714f281f385fa8a8744d96e843cf85f
karambaq <0maximq@gmail.com>
2017-09-26 14:16:06 +0300

    upda

--- main.py
+++ main.py
@@ -81,6 +81,24 @@ class MainWindow(QWidget):
         self.color_picker.resize(190, 25)
         self.color_picker.clicked.connect(self.color_dialog_on_click)
 
+        self.scale_from = QTextEdit(self)
+        self.scale_from.move(50, 470)
+        self.scale_from.resize(80, 25)
+        self.scale_from.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.scale_to = QTextEdit(self)
+        self.scale_to.move(150, 470)
+        self.scale_to.resize(80, 25)
+        self.scale_to.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.choose_file = QPushButton('Choose File', self)
+        self.choose_file.move(50, 500)
+        self.choose_file.clicked.connect(self.choose_file_on_click)
+
+        self.scale = QPushButton('Scale File', self)
+        self.scale.move(150, 500)
+        self.scale.clicked.connect(self.scale_on_click)
+
         self.set_fonts()
 
         self.show()
@@ -97,28 +115,32 @@ class MainWindow(QWidget):
         self.border1s.setFont(QFont(font, 11))
         self.border_from.setFont(QFont(font, 11))
         self.border_to.setFont(QFont(font, 11))
+        self.choose_file.setFont(QFont(font, 11))
+        self.scale.setFont(QFont(font, 11))
+        self.scale_from.setFont(QFont(font, 11))
+        self.scale_to.setFont(QFont(font, 11))
 
     def color_dialog_on_click(self):
         self.open_color_dialog()
 
     def open_color_dialog(self):
-        col = QColorDialog.getColor()
-        r = col.red()
-        g = col.green()
-        b = col.blue()
+        color = QColorDialog.getColor()
+        r = color.red()
+        g = color.green()
+        b = color.blue()
+        # self.col = color.
         self.col = self.to_grey_shadow(r, g, b)
 
     def to_grey_shadow(self, r, g, b):
-        return 0.2126 * r + 0.7152 * g + 0.0722 * b
+        return r * 299/1000 + g * 587/1000 + b * 114/1000
 
     def to_0s_and_1s_on_click(self):
-        col = Image.open(self.filename)
-        gray = col.convert('L')
+        color = Image.open(self.filename)
+        gray = color.convert('L')
         bw = np.asarray(gray).copy()
         border = int(self.border.toPlainText())
-
-        bw[abs(bw - self.col) < border] = 0  # Black
-        bw[abs(bw - self.col) >= border] = 255  # White
+        bw[abs(bw[...] - self.col) >= border] = 0
+        bw[abs(bw[...] - self.col) < border] = 255
 
         img = Image.fromarray(bw)
         self.label.setPixmap(img.toqpixmap().scaled(670, 500))
@@ -132,8 +154,9 @@ class MainWindow(QWidget):
         gray = col.convert('L')
         bw = np.asarray(gray).copy()
         border = int(self.border.toPlainText())
-        bw[abs(bw - self.col) < border] = 0  # Black
-        bw[abs(bw - self.col) >= border] = 255  # White
+
+        bw[abs(bw[...] - self.col) >= border] = 0
+        bw[abs(bw[...] - self.col) < border] = 255
 
         b = bw.astype(int)
         b[b == 0] = -1
@@ -149,11 +172,33 @@ class MainWindow(QWidget):
         gray = col.convert('L')
         bw = np.asarray(gray).copy()
         left_bound = int(self.border_from.toPlainText())
+        print(left_bound)
         right_bound = int(self.border_to.toPlainText())
-        sc = 256 / (left_bound + right_bound)
+        sc = (right_bound - left_bound) / 255
         ca = bw.astype(float).copy()
-        ra = ca / sc
-        np.savetxt('after_a_b.txt', ra, fmt='%f', delimiter=",")
+        ra = left_bound + (255 - ca) * sc
+        np.savetxt('after_a_b.txt', ra, fmt='%.2f', delimiter=",")
+
+    def scale_on_click(self):
+        scale = (int(self.scale_to.toPlainText()) - int(self.scale_from.toPlainText())) * 1.0 / (max(self.data) - min(self.data))
+        ret_data = []
+        for i in range(len(self.data)):
+            c = int(self.scale_from.toPlainText()) + (self.data[i] - min(self.data)) * scale
+            ret_data.append(c)
+        with open('scale_data.txt', 'w') as file:
+            for i in ret_data:
+                file.write(str(i))
+                file.write(' ')
+
+    def choose_file_on_click(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.scale_filename, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+                                                       "Txt files (*.txt)", options=options)
+        with open(self.scale_filename) as file:
+            self.data = file.read().split(',')
+        self.data = [x.strip(' ') for x in self.data]
+        self.data = [int(x) for x in self.data]
 
     def open_file_name_dialog(self):
         options = QFileDialog.Options()
